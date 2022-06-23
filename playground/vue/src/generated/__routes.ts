df179cfa33b3649fb140524dae238cf8dd5b0beb
Victor Garcia <victor.garcia@malt.com>
2022-06-23 18:23:37 +0200

    fix ci

--- playground/vue/src/generated/__routes.ts
+++ playground/vue/src/generated/__routes.ts
@@ -4,6 +4,32 @@
  * ---------------------
  * */
 
+export const routesNames = {
+  activate: 'activate' as const,
+  index: 'index' as const,
+  childOne: {
+    childOneChildOneSubOne: 'parent-child-one-child-one-sub-one' as const,
+    user: { oneChildOneSubOneUserId: 'parent-child-one-child-one-sub-one-user-id' as const },
+    childOneChildOneSubTwo: 'parent-child-one-child-one-sub-two' as const,
+    index: 'parent-child-one' as const,
+  },
+  childTwo: {
+    childTwoId: 'parent-child-two-id' as const,
+    childTwoChildOneSubOneUser: 'parent-child-two-child-one-sub-one-user' as const,
+    childTwoChildOneSubOne: 'parent-child-two-child-one-sub-one' as const,
+    index: 'parent-child-two' as const,
+    profile: {
+      id: {
+        profileIdSlug: 'parent-child-two-profile-id-slug' as const,
+        index: 'parent-child-two-profile-id' as const,
+      },
+      index: 'parent-child-two-profile' as const,
+    },
+  },
+  rootPage: 'rootPage' as const,
+  test: 'test' as const,
+};
+
 export type TypedRouteList =
   | 'activate'
   | 'index'
--- playground/vue/vite.config.ts
+++ playground/vue/vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue';
 import TypedRouter from '../../dist';
 
 export default defineConfig({
-  plugins: [vue(), TypedRouter({printRoutesTree: false})],
+  plugins: [vue(), TypedRouter()],
 });
